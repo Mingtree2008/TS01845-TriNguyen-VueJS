488e42c7c35c9bada822e855ede3ac9475f13b76
Mingtree2008 <tn098738@gmail.com>
2026-01-31 00:33:13 +0700

    update

---
+++ Slide6/src/components/Bai2.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h3>Nhập tháng của bạn:</h3>
+        <input type="month" v-model="month"  />
+        <p v-if="month <1 || month >12" style="color: red;">Vui lòng nhập tháng hợp lệ (1-12)</p>
+        <p v-else-if="month >= 1 && month <= 3">Mùa xuân</p>
+        <p v-else-if="month >= 4 && month <= 6">Mùa hạ</p>
+        <p v-else-if="month >= 7 && month <= 9">Mùa thu</p>
+        <p v-else>Mùa đông</p>
+    </div>
+</template>
+<script setup>
+import { ref } from 'vue';
+const month = ref(0);
+</script>
